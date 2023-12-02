c9455565ae68020a72e6399729aad29180c39afa
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-12-02 11:57:49 +0200

    Restructured the code

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import day2.day2 as current_day
 
 
 def main():
-    part1_input: str = read_file(f"{current_day.get_name()}/input/part1.input.small")
-    part2_input: str = read_file(f"{current_day.get_name()}/input/part2.input.small")
-    input: str = read_file(f"{current_day.get_name()}/input/input")
+    day_name = current_day.get_name()
 
-    part1_output_small = current_day.part1(part1_input, )
-    part1_output = current_day.part1(input)
+    part1_input_small: str = read_file(f"{day_name}/input/part1.input.small")
+    part1_input: str = read_file(f"{day_name}/input/input")
 
-    part2_output_small = current_day.part2(part2_input)
-    part2_output = current_day.part2(input)
+    part2_input_small: str = read_file(f"{day_name}/input/part2.input.small")
+    part2_input: str = read_file(f"{day_name}/input/input")
 
-    write_file(f"{current_day.get_name()}/output/part1.output.small", part1_output_small)
-    write_file(f"{current_day.get_name()}/output/part1.output", part1_output)
-    write_file(f"{current_day.get_name()}/output/part2.output.small", part2_output_small)
-    write_file(f"{current_day.get_name()}/output/part2.output", part2_output)
+    part1_output_small = current_day.part1(part1_input_small) if part1_input_small != "" else ""
+    part1_output = current_day.part1(part1_input) if part1_input != "" else ""
+
+    part2_output_small = current_day.part2(part2_input_small) if part2_input_small != "" else ""
+    part2_output = current_day.part2(part2_input) if part2_input != "" else ""
+
+    write_file(f"{day_name}/output/part1.output.small", part1_output_small)
+    write_file(f"{day_name}/output/part1.output", part1_output)
+
+    write_file(f"{day_name}/output/part2.output.small", part2_output_small)
+    write_file(f"{day_name}/output/part2.output", part2_output)
 
 
 if __name__ == '__main__':
